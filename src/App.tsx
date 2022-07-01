44863bd1c900c4f46c7f070128ef33f0d0a00466
Marlena Śliwińska <marlena.sliwinska@best.com.pl>
2022-07-01 20:12:18 +0200

    hero initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { Skills } from './components/Skills';
+import { Hero } from './components/Hero';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <Hero />
     <Skills />
     {/*
       <Box textAlign="center" fontSize="xl">
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { chakra, Container, Stack, Text, Box } from '@chakra-ui/react';
+import WorkingWoman from '../Icons/WorkingWoman';
+
+const HeroSection = () => {
+  console.log('hero section');
+  return (
+    <Container maxW="6xl" px={{ base: 6, md: 3 }} py={24}>
+      <Stack direction={{ base: 'column', md: 'row' }} justifyContent="center">
+        <Stack
+          direction="column"
+          spacing={6}
+          justifyContent="center"
+          maxW="480px"
+        >
+          <chakra.h1
+            fontSize="5xl"
+            lineHeight={1}
+            fontWeight="bold"
+            textAlign="left"
+          >
+            Marlena Śliwińska
+            <br />
+            <chakra.span color="teal">FRONTEND DEVELOPER</chakra.span>
+          </chakra.h1>
+          <Text
+            fontSize="1.2rem"
+            textAlign="left"
+            lineHeight="1.375"
+            fontWeight="400"
+            color="gray.500"
+          >
+            If you are looking for an ambitious, intelligent and passionate
+            co-worker, you are in the right place. If you have any questions
+            don&apos;t hesitate to contact me.
+          </Text>
+        </Stack>
+        <Box
+          ml={{ base: 0, md: 5 }}
+          maxW={{ base: 'xs', md: '6xl' }}
+          pos="relative"
+        >
+          <WorkingWoman />
+        </Box>
+      </Stack>
+    </Container>
+  );
+};
+
+export default HeroSection;
